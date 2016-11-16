3df6c58db38078f95abd41488c3459337cc3f18c
drbgfc <drb.gfc@comcast.net>
2016-11-16 14:27:24 -0500

    Set categoryRubrics to empty array for failing sections to match format

--- src/main/java/org/sitenv/service/ccda/smartscorecard/model/Category.java
+++ src/main/java/org/sitenv/service/ccda/smartscorecard/model/Category.java
@@ -1,14 +1,16 @@
 package org.sitenv.service.ccda.smartscorecard.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
 	
 	
-	public Category(boolean isFailingConformance,String categoryName)
+	public Category(boolean isFailingConformance, String categoryName)
 	{
 		this.isFailingConformance = isFailingConformance;
 		this.categoryName = categoryName;
+		this.categoryRubrics = new ArrayList<CCDAScoreCardRubrics>();
 	}
 	
 	public Category()
@@ -25,14 +27,6 @@ public class Category {
 	
 	// referenceccdavalidator properties
 	private boolean isFailingConformance;
-	// e.g. description, xPath, Line Number, etc.
-	// TODO: this is convenient here but we don't NEED it but it makes for less front-end processing
-	// Options:
-	// 1. We populate this (referenceErrors)
-	// 2. We override categoryRubrics with the related reference results
-	// 3. We do not populate this, and delete it from the class. Instead, we have the front end 
-	// loop the results in the current ReferenceResult instance and match the section name to populate 
-	private List<ReferenceError> referenceErrors;
 	
 	public String getCategoryName() {
 		return categoryName;
@@ -69,12 +63,6 @@ public class Category {
 	}
 	public void setFailingConformance(boolean isFailingConformance) {
 		this.isFailingConformance = isFailingConformance;
-	}
-	public List<ReferenceError> getReferenceErrors() {
-		return referenceErrors;
-	}
-	public void setReferenceErrors(List<ReferenceError> referenceErrors) {
-		this.referenceErrors = referenceErrors;
 	}	
 	
 }
