943a71443cfe84896995b2d387bfb04520116425
drbgfc <drb.gfc@comcast.net>
2016-11-22 11:11:34 -0500

    Revert to windows new line format for debug test data

--- src/main/webapp/MainController.js
+++ src/main/webapp/MainController.js
@@ -5,7 +5,7 @@ scApp.controller('MainController', ['$scope', '$location', '$anchorScroll', '$ti
 	};
 	
 	$scope.mainDebug = {
-		inDebugMode: true,
+		inDebugMode: false,
 		useLocalTestDataForServices: false
 	};
 	
