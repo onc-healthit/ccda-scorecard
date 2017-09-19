07d0717dfe768a57e961638a67068db1f9c5e176
drbgfc <drb.gfc@comcast.net>
2017-09-19 16:23:06 -0400

    Add constant containing sentences for "what to do" One Click schema

--- src/main/java/org/sitenv/service/ccda/smartscorecard/util/ApplicationConstants.java
+++ src/main/java/org/sitenv/service/ccda/smartscorecard/util/ApplicationConstants.java
@@ -364,6 +364,9 @@ public class ApplicationConstants {
 				+ "urn:hl7ii:2.16.840.1.113883.10.20.22.1.10. The C-CDA Scorecard tool does not score this document type. "
 				+ "Please try submitting another document type for review such as a Continuity of Care Document, Care Plan, etc.";
 		public static final String SCHEMA_ERRORS_GENERIC = "HL7 CDA Schema Errors must be addressed before a score can be provided.";
+		public static final String SCHEMA_ERRORS_GENERIC_PART2 = "Please provide the results below to your vendor for a resolution. "
+				+ "Once the schema errors in your document are fixed, "
+				+ "it can be resubmitted to the ONC One Click Scorecard for a full report.";
 	}
 
 	public static enum IG_REFERENCES
