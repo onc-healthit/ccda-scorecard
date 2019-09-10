462ca3f56d0a245b1399561ebd5048bcbef2f867
drbgfc <dbrown.se@gmail.com>
2019-09-10 19:23:03 -0400

    Update ref val dev server URL

--- src/main/java/org/sitenv/service/ccda/smartscorecard/cofiguration/ApplicationConfiguration.java
+++ src/main/java/org/sitenv/service/ccda/smartscorecard/cofiguration/ApplicationConfiguration.java
@@ -26,9 +26,9 @@ public class ApplicationConfiguration {
 	
 	// set DEFAULT_LOCAL_SERVER_URL according to local tomcat URL
 	public static final String DEFAULT_LOCAL_SERVER_URL = "http://localhost:8000",
-			CCDA_DEV_SERVER_URL = "https://devccda.sitenv.org",
+			CCDA_DEV_SERVER_URL = "https://devccda.sitenv.org", // TODO: update this url to the HHS server we will be using
 			CCDA_PROD_SERVER_URL = "https://prodccda.sitenv.org",
-			TTP_DEV_SERVER_URL = "https://ttpdstest.sitenv.org:8443",
+			TTP_DEV_SERVER_URL = "52.44.175.145/referenceccdaservice", // HHS dev James
 			TTP_PROD_SERVER_URL = "https://ttpds.sitenv.org:8443",
 			CODE_AND_DISPLAYNAME_IN_CODESYSTEM_SERVICE = "/referenceccdaservice/iscodeandisplaynameincodesystem",
 			CODE_IN_VALUESET_SERVICE = "/referenceccdaservice/iscodeinvalueset",
